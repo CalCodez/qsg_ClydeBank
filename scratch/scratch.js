ed60410d61ae883ba41af6e5606965253f143a4a
Calvin <calvincodez@gmail.com>
2025-12-03 09:04:38 -0600

    coffeeshop example clikced update

--- scratch/scratch.js
+++ scratch/scratch.js
@@ -1,3 +1,4 @@
+//++Scratch File Example
 const firstNameTextBox = document.getElementById('firstName');
 const submitButton = document.getElementById('submit');
 const greetingContainer = document.getElementById('greeting');
@@ -6,9 +7,25 @@ submitButton.addEventListener('click', () => {
 	greetingContainer.textContent = `Hello, ${firstNameTextBox.value}!`;
 });
 
+//++Switch File Example
 const colorSelector = document.getElementById('colorSelector');
 const colorSquare = document.getElementById('colorSquare');
 
-colorSelector = addEventListener('change', () => {
+colorSelector.addEventListener('change', () => {
 	colorSquare.style.backgroundColor = colorSelector.value;
 });
+
+//++Clicked File Example
+let theButton = document.getElementById('theButton');
+
+theButton.addEventListener(
+	'click',
+	(function () {
+		let clickCount = 0;
+		return function () {
+			clickCount += 1;
+
+			this.textContent = `Clicked ${clickCount} times!`;
+		};
+	})()
+);
