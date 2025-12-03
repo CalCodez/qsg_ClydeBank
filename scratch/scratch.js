9e6c58d6dd52d9da8772b5ad78f9934c5969220c
Calvin <calvincodez@gmail.com>
2025-12-03 14:12:28 -0600

    switch example 2

--- scratch/scratch.js
+++ scratch/scratch.js
@@ -29,3 +29,45 @@ theButton.addEventListener(
 		};
 	})()
 );
+
+//++Switch File Example 2
+const colorSelector2 = {
+	colors: {
+		red: '#ff0000',
+		orange: '#ffa500',
+		yellow: '#ffff00',
+		green: '#008000',
+		blue: '#0000ff',
+		indigo: '#4b0082',
+		violet: '#ee82ee',
+	},
+
+	render: (parent, swatch, id = '', className = '') => {
+		let select = document.createElement('select');
+
+		//``If there was an ID this would assign the ID to the select ELement
+		if (id) {
+			select.id = id;
+		}
+
+		//``If three was an className this would assign the className to the select Element.
+		if (className) {
+			select.className = className;
+		}
+
+		for (let color in colorSelector2.colors) {
+			let option = document.createElement('option');
+			option.value = colorSelector2.colors[color];
+			option.innerHTML = color.charAt(0).toUpperCase() + color.slice(1);
+			select.appendChild(option);
+		}
+		select.addEventListener('change', (e) => {
+			swatch.style.backgroundColor = e.target.value;
+		});
+		parent.appendChild(select);
+	},
+};
+
+let colorSwatch = document.getElementById('colorSwatch');
+let colorSelection = document.getElementById('colorSelection');
+colorSelector2.render(colorSelection, colorSwatch);
